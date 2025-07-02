dda208bb755e0afb79ec2473c6842762ad6601a6
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2025-07-02 22:11:33 +0900

    fix: :bug: 20250702 ホットリロードが反応していなかったバグを修正

--- node_vite/vite.config.js
+++ node_vite/vite.config.js
@@ -4,7 +4,13 @@ export default {
         host: '0.0.0.0', // 明示的に書く
         port: 5173,
         watch: {
-            usePolling: true // ← 重要（特にWindows + Dockerで必要！）
+            usePolling: true, // ← 重要（特にWindows + Dockerで必要！）
+            interval: 100, // ポーリング間隔（ミリ秒）
+        },
+        hmr: {
+            host: 'localhost', // ホスト名を明示的に指定
+            port: 5173, // HMRのポート番号
+            protocol: 'ws', // WebSocketプロトコルを使用
         },
     }
 }
